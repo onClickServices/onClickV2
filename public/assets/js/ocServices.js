d668d1eed822c43d969c136cefb318b67dd7409f
Perry Craft <LjCraft12@gmail.com>
2018-05-31 22:08:35 -0400

    Completed design modal

---
+++ public/assets/js/ocServices.js
@@ -0,0 +1,18 @@
+let Users = require('lib/models/user');
+
+$(function() {
+
+    var welcomeSection = $('.welcome-section'),
+        enterButton = welcomeSection.find('.enter-button');
+
+    setTimeout(function() {
+        welcomeSection.removeClass('content-hidden');
+    }, 500);
+
+    enterButton.on('click', function() {
+        e.preventDefault();
+        welcomeSection.addClass('content-hidden').fadeOut();
+    });
+
+
+})();
